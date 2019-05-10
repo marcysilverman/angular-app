557bffa3d5ebca626f6946db9667eead23b99881
Marcy Silverman <marcysilve225@gmail.com>
2019-05-10 12:52:39 -0700

    added a header and router is working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'to-do-n7';
+  title = 'My First Angular App';
 }
